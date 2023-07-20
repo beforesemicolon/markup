0409efec0cb6888af9b9f8cd197802730c7f4d31
Elson Correia <design@elsoncorreia.com>
2023-07-19 20:23:44 -0400

    improve list rendering algorithm to do less insertBefore and group appends

--- src/executable/change-current-into-new-items.ts
+++ src/executable/change-current-into-new-items.ts
@@ -7,71 +7,32 @@
  * @param endNode - an anchored node placed right after the last of the currentlyRendered nodes
  */
 export const changeCurrentIntoNewItems = (currentChildNodes: Node[], newChildNodes: Node[], endNode: Node) => {
-	const newChildNodesSet = new Set(newChildNodes);
+	const currentChildNodesSet = new Set(currentChildNodes);
+	let frag: DocumentFragment = document.createDocumentFragment();
 	
-	if (currentChildNodes.length && newChildNodes.length) {
-		const notRemovedCurrentItems: Node[] = [];
-		
-		for (let currentItem of currentChildNodes) {
-			if (!newChildNodesSet.has(currentItem)) {
-				currentItem.parentNode?.removeChild(currentItem);
-			} else {
-				notRemovedCurrentItems.push(currentItem)
-			}
-		}
-		
-		/**
-		 * handle case where some nodes are left rendered
-		 * otherwise is the same as adding all new nodes
-		 */
-		if (notRemovedCurrentItems.length) {
-			let ci = 0;
-			let ni = 0;
-			const insertedNodesSet: Set<Node> = new Set()
+	newChildNodes.forEach((n, i) => {
+		const moved = currentChildNodes[i] !== n;
+		if(moved || !currentChildNodesSet.has(n)) {
+			frag.appendChild(n);
 			
-			while(ni < newChildNodes.length) {
-				const c = notRemovedCurrentItems[ci];
-				const n = newChildNodes[ni];
-				
-				/**
-				 * if a node was previously inserted, move the needle to the next current node
-				 * never inserted and resume the comparison
-				 */
-				if(insertedNodesSet.has(c)) {
-					ci += 1;
-					continue;
-				}
-				
-				/**
-				 * if nodes are the same and the same position move the needles
-				 * otherwise move the new node right in front and move to the next new node
-				 */
-				if(c !== n) {
-					(c || endNode)?.parentNode?.insertBefore(n, c);
-					insertedNodesSet.add(n);
-					ni += 1;
-				} else {
-					ci += 1;
-					ni += 1;
-				}
+			if (moved) {
+				currentChildNodesSet.delete(n);
+			}
+		} else {
+			if (frag.childNodes.length) {
+				n.parentNode?.insertBefore(frag as DocumentFragment, n);
+				frag = document.createDocumentFragment();
 			}
 			
-			return;
+			currentChildNodesSet.delete(n);
 		}
-		
-		currentChildNodes = [];
+	})
+	
+	if (frag.childNodes.length) {
+		endNode.parentNode?.insertBefore(frag as DocumentFragment, endNode);
 	}
 	
-	if(newChildNodes.length) {
-		/**
-		 * if no current items, just insert all the new items in the order they came
-		 */
-		newChildNodes.forEach(n => {
-			endNode.parentNode?.insertBefore(n, endNode)
-		})
-	} if(currentChildNodes.length) {
-		currentChildNodes.forEach(n => {
-			n?.parentNode?.removeChild(n)
-		})
-	}
+	currentChildNodesSet.forEach(c => {
+		c?.parentNode?.removeChild(c)
+	})
 }
