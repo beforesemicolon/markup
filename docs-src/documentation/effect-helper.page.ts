fa3d2f906a316ee4a3e0b7978f5fc89b5a02e337
Elson Correia <design@elsoncorreia.com>
2023-11-04 11:20:27 -0400

    effect doc

--- docs-src/documentation/effect-helper.page.ts
+++ docs-src/documentation/effect-helper.page.ts
@@ -6,4 +6,81 @@ import { CodeSnippet } from '../partials/code-snippet'
 
 const page = HelpersGroup.list[1]
 
-export default DocPageLayout(page.path, html`${Heading(page.name)}`)
+export default DocPageLayout(
+    page.path,
+    html`
+        ${Heading(page.name)}
+        <p>
+            When using <a href="./state-values.html">state</a> you will notice
+            that only the part of the template dependent on the state will
+            update with changes.
+        </p>
+        <p>
+            For those situations you would want to perform "side effects" on the
+            template, meaning, you want other part of the template to change
+            with some state change.
+        </p>
+        <p>
+            That's when the <code>effect</code> helper comes in. To help you
+            perform these side effects.
+        </p>
+        ${CodeSnippet(
+            'const counter = html`\n' +
+                '  <p>${count}</p>\n' +
+                '  <p>${effect(count, () => count() * 2)}</p>\n' +
+                '`;'
+        )}
+        <p>
+            The above example shows a simple example of performing a side
+            effect. The bellow example show using <code>effect</code> we can
+            react to the <code>activeTab</code>
+            state change to get the content and render it.
+        </p>
+        ${CodeSnippet(
+            "type Tab = 'home' | 'about' | 'contact';\n\n" +
+                'const tabContent: Record<Tab, HTMLTemplate> = {\n' +
+                '  home: html`<h1>Welcome</h1>`,\n' +
+                '  contact: html`<p>Tell us what you think</p>`,\n' +
+                '  about: html`<p>We are here to help you</p>`,\n' +
+                '}\n' +
+                '\n' +
+                'const [activeTab, setActiveTab] = state<Tab>("home");\n' +
+                '\n' +
+                'const app = html`\n' +
+                '  <h1>Tab Example</h1>\n' +
+                '  <ul>\n' +
+                '    <li \n' +
+                '      attr.class.active="${is(activeTab, \'home\')}" \n' +
+                '      onclick="${() => setActiveTab(\'home\')}">\n' +
+                '      Home\n' +
+                '    </li>\n' +
+                '    <li \n' +
+                '      attr.class.active="${is(activeTab, \'about\')}"\n' +
+                '      onclick="${() => setActiveTab(\'about\')}">\n' +
+                '      About\n' +
+                '    </li>\n' +
+                '    <li \n' +
+                '      attr.class.active="${is(activeTab, \'contact\')}"\n' +
+                '      onclick="${() => setActiveTab(\'contact\')}">\n' +
+                '      Contact\n' +
+                '    </li>\n' +
+                '  </ul>\n' +
+                '  ${effect(activeTab, () => tabContent[activeTab()])}\n' +
+                '`;\n' +
+                '\n' +
+                'app.render(document.body)',
+            'typescript'
+        )}
+        ${Heading('Effect format', 'h3')}
+        <p>
+            The <code>effect</code> take comma-separated states and as the last
+            argument should be the action to take or the "thing" you need to be
+            returned.
+        </p>
+        <p><code>effect(STATE, ...STATE, SIDE_EFFECT_ACTION)</code></p>
+        ${CodeSnippet(
+            'effect(activeTab, currentPath, contentLoader)',
+            'typescript'
+        )}
+    `
+)
