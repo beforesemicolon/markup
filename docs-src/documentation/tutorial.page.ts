83bca9c68e43b496b438d53c09e73fca05a31692
Elson Correia <design@elsoncorreia.com>
2023-11-08 21:27:57 -0500

    mobile menu styling

--- docs-src/documentation/tutorial.page.ts
+++ docs-src/documentation/tutorial.page.ts
@@ -2,8 +2,76 @@ import { html } from '../../src'
 import { DocPageLayout } from '../partials/doc-page-layout'
 import { IntroGroup } from '../data/documents'
 import { Heading } from '../partials/heading'
-import { CodeSnippet } from '../partials/code-snippet'
+import { PlaylistContent } from '../partials/playlist-content'
+import tutorialTraining from '../data/tutorial.json'
 
 const page = IntroGroup.list[3]
 
-export default DocPageLayout(page.path, html`${Heading(page.name)}`)
+export default DocPageLayout(
+    page.path,
+    html`${Heading(page.name)} ${PlaylistContent(tutorialTraining)}`
+)
+
+// const reordableList = helper(
+// 	(list, renderItem, renderContainer, withDrop) => {
+// 		const placeholder = element('div', {
+// 			attributes: {
+// 				style: 'background: cyan; height: 3px; border-radius: 1px;'
+// 			}
+// 		})
+// 		let draggingElement = null;
+// 		let prevY = 0;
+//
+// 		const onDragOver = e => {
+// 			e.preventDefault();
+// 			const draggingOverElement = e.target.closest('[draggable][data-order]');
+//
+// 			if(draggingOverElement && draggingOverElement !== draggingElement) {
+// 				if (e.pageY < prevY) {
+// 					draggingOverElement.before(placeholder)
+// 				} else if (e.pageY > prevY) {
+// 					draggingOverElement.after(placeholder)
+// 				}
+// 			}
+//
+// 			prevY = e.pageY;
+// 		}
+//
+// 		const onDragStart = (e) => {
+// 			draggingElement = e.currentTarget;
+// 		}
+//
+// 		const onDrop = (e) => {
+// 			if(placeholder.isConnected) {
+// 				placeholder.parentNode.replaceChild(draggingElement, placeholder)
+//
+// 				if(container) {
+// 					withDrop?.(
+// 						Array.from(container.querySelectorAll('[draggable][data-order]'), el => el.dataset.order)
+// 					)
+// 				}
+// 			}
+// 		}
+//
+// 		const container = renderContainer();
+//
+// 		container.addEventListener('dragover', onDragOver)
+// 		container.addEventListener('drop', onDrop)
+//
+// 		const temp = html`
+//       ${repeat(list, (item, idx) => {
+// 			const el = renderItem(item);
+//
+// 			el.setAttribute('draggable', 'true')
+// 			el.dataset.order = idx;
+// 			el.addEventListener('dragstart', onDragStart)
+//
+// 			return el;
+// 		})}
+//     `;
+//
+// 		temp.render(container);
+//
+// 		return () => container;
+// 	}
+// )
--- docs-src/partials/doc-page-layout.ts
+++ docs-src/partials/doc-page-layout.ts
@@ -20,34 +20,40 @@ export const DocPageLayout = (currentPage: string, content: HtmlTemplate) =>
         basePath: '../',
         content: html`
             <div class="wrapper docs">
-                <ul class="docs-list" tabindex="0">
-                    ${[
-                        IntroGroup,
-                        TemplatingGroup,
-                        HelpersGroup,
-                        ComponentsGroup,
-                    ].map(
-                        (g) => html`
-                            <li>
-                                <span>${g.name}</span>
-                                <ul>
-                                    ${g.list.map(
-                                        (l) => html`
-                                            <li
-                                                attr.class.active="${l.path ===
-                                                currentPage}"
-                                            >
-                                                <a href="../${l.path}"
-                                                    >${l.name}</a
+                <div class="doc-nav" id="menu">
+                    <a class="mobile-menu-open" href="#menu">
+                        <span>menu</span>
+                    </a>
+                    <ul class="doc-nav-list" id="doc-nav-list" tabindex="0">
+                        ${[
+                            IntroGroup,
+                            TemplatingGroup,
+                            HelpersGroup,
+                            ComponentsGroup,
+                        ].map(
+                            (g) => html`
+                                <li>
+                                    <span>${g.name}</span>
+                                    <ul>
+                                        ${g.list.map(
+                                            (l) => html`
+                                                <li
+                                                    attr.class.active="${l.path ===
+                                                    currentPage}"
                                                 >
-                                            </li>
-                                        `
-                                    )}
-                                </ul>
-                            </li>
-                        `
-                    )}
-                </ul>
+                                                    <a href="../${l.path}"
+                                                        >${l.name}</a
+                                                    >
+                                                </li>
+                                            `
+                                        )}
+                                    </ul>
+                                </li>
+                            `
+                        )}
+                    </ul>
+                    <a href="../${currentPage}" class="mobile-menu-close"></a>
+                </div>
                 <article>${content}</article>
             </div>
         `,
