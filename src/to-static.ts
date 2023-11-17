caa30985c9e662f5ae1124b4f6415064ae3405af
Elson Correia <design@elsoncorreia.com>
2023-11-17 16:12:12 -0500

    add doctype option to toStatic

--- src/to-static.ts
+++ src/to-static.ts
@@ -2,9 +2,9 @@ import 'global-jsdom/register'
 import { HtmlTemplate } from './html'
 import { doc } from './doc'
 
-export const toStatic = (temp: HtmlTemplate) => {
+export const toStatic = (temp: HtmlTemplate, docType = '<!doctype html>') => {
     doc.body.innerHTML = ''
     temp.render(doc.body)
 
-    return doc.body.innerHTML.trim()
+    return docType + doc.body.innerHTML.trim()
 }
