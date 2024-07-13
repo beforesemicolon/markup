2db502966828fab6ee6eb7b9a5b9d24349344d8d
Elson Correia <design@elsoncorreia.com>
2024-07-13 16:44:03 -0400

    split attribute directive into small functions

--- src/utils/set-element-directive-attribute.ts
+++ src/utils/set-element-directive-attribute.ts
@@ -35,83 +35,116 @@ export const setElementDirectiveAttribute = (
     const valueParts = data.split(/\|/)
     const [value, condition] = valueParts.map((s) => s.trim())
     const parsedValue = simpleParseString(value)
-    let shouldAdd =
+    const shouldAdd =
         valueParts.length > 1 ? simpleParseString(condition) : parsedValue
 
     if (attrName === 'style') {
-        if (property) {
-            element.style.setProperty(property, shouldAdd ? value : '')
-        } else {
-            for (const style of value.match(/([a-z][a-z-]+)(?=:):([^;]+)/g) ??
-                []) {
-                const [name, styleValue] = style.split(':').map((s) => s.trim())
-
-                if (shouldAdd) {
-                    element.style.setProperty(name, styleValue)
-                } else {
-                    element.style.removeProperty(name)
-                }
-            }
-        }
-
-        if (!element.style.length) {
-            element.removeAttribute('style')
-        }
+        setElementStyle(property, value, shouldAdd, element)
     } else if (attrName === 'class') {
-        if (property) {
-            if (shouldAdd) {
-                element.classList.add(property)
-            } else {
-                element.classList.remove(property)
-            }
-        } else {
-            // ''.split(/\s+/g) results in [''] which will fail in classList actions
-            for (const cls of value ? value.split(/\s+/g) : []) {
-                if (shouldAdd) {
-                    element.classList.add(cls)
-                } else {
-                    element.classList.remove(cls)
-                }
-            }
-        }
-
-        if (!element.classList.length) {
-            element.removeAttribute('class')
-        }
+        setElementClass(property, value, shouldAdd, element)
     } else if (attrName === 'data') {
-        if (property) {
-            if (shouldAdd) {
-                element.dataset[turnKebabToCamelCasing(property)] = value
-            } else {
-                element.removeAttribute(
-                    `data-${turnCamelToKebabCasing(property)}`
-                )
-            }
-        }
+        setElementDataAttribute(property, value, shouldAdd, element)
     } else if (attrName) {
-        const boolAttr = booleanAttributes[attrName]
-        const boolAttributeWithValidPossibleValue =
-            boolAttr?.possibleValues && boolAttr.possibleValues.includes(value)
-        // when it's a native boolean attribute, the value itself should be the flag to whether add/remove it
-        // unless it is a boolean attribute with a valid possible value
-        shouldAdd =
-            boolAttr &&
-            !boolAttributeWithValidPossibleValue &&
-            typeof parsedValue === 'boolean'
-                ? parsedValue
-                : shouldAdd
+        setElementBooleanAttribute(
+            attrName,
+            value,
+            parsedValue,
+            shouldAdd,
+            element
+        )
+    }
+}
 
+function setElementBooleanAttribute(
+    attrName: string,
+    value: string,
+    parsedValue: unknown,
+    shouldAdd: unknown,
+    element: HTMLElement
+) {
+    const boolAttr = booleanAttributes[attrName]
+    const boolAttributeWithValidPossibleValue =
+        boolAttr?.possibleValues?.includes(value)
+    // when it's a native boolean attribute, the value itself should be the flag to whether add/remove it
+    // unless it is a boolean attribute with a valid possible value
+    shouldAdd =
+        boolAttr &&
+        !boolAttributeWithValidPossibleValue &&
+        typeof parsedValue === 'boolean'
+            ? parsedValue
+            : shouldAdd
+
+    if (shouldAdd) {
+        setElementAttribute(element, attrName, value)
+    } else {
+        element.removeAttribute(attrName)
+    }
+}
+
+function setElementDataAttribute(
+    property: string,
+    value: string,
+    shouldAdd: unknown,
+    element: HTMLElement
+) {
+    if (property) {
         if (shouldAdd) {
-            if (boolAttr) {
-                element.setAttribute(
-                    attrName,
-                    boolAttributeWithValidPossibleValue ? value : 'true'
-                )
-            } else {
-                setElementAttribute(element, attrName, value)
-            }
+            element.dataset[turnKebabToCamelCasing(property)] = value
         } else {
-            element.removeAttribute(attrName)
+            element.removeAttribute(`data-${turnCamelToKebabCasing(property)}`)
         }
+    }
+}
+
+function setElementClass(
+    property: string,
+    value: string,
+    shouldAdd: unknown,
+    element: HTMLElement
+) {
+    if (property) {
+        if (shouldAdd) {
+            element.classList.add(property)
+        } else {
+            element.classList.remove(property)
+        }
+    } else {
+        // ''.split(/\s+/g) results in [''] which will fail in classList actions
+        for (const cls of value ? value.split(/\s+/g) : []) {
+            if (shouldAdd) {
+                element.classList.add(cls)
+            } else {
+                element.classList.remove(cls)
+            }
+        }
+    }
+
+    if (!element.classList.length) {
+        element.removeAttribute('class')
+    }
+}
+
+function setElementStyle(
+    property: string,
+    value: string,
+    shouldAdd: unknown,
+    element: HTMLElement
+) {
+    if (property) {
+        element.style.setProperty(property, shouldAdd ? value : '')
+    } else {
+        for (const style of value.match(/([a-z][a-z-]+)(?=:):([^;]+)/g) ?? []) {
+            const [name, styleValue] = style.split(':').map((s) => s.trim())
+
+            if (shouldAdd) {
+                element.style.setProperty(name, styleValue)
+            } else {
+                element.style.removeProperty(name)
+            }
+        }
+    }
+
+    if (!element.style.length) {
+        element.removeAttribute('style')
     }
 }
