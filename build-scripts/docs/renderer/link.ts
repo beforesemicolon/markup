9484d580025a07c270d978749ad800ffd65f1e8d
Elson Correia <design@elsoncorreia.com>
2024-10-27 22:48:59 -0400

    use @beforesemicolon/builder

--- build-scripts/docs/renderer/link.ts
+++
@@ -1,7 +0,0 @@
-import { Tokens } from 'marked'
-
-export default function link({ href, text }: Tokens.Link) {
-    return `<a href="${href
-        .replace(/\.md/, '.html')
-        .replace(/index\.html/, '')}">${text}</a>`
-}
