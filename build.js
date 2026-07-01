4f1ffc465c688c577b1b60602f45e53427c0f6f9
Elson Correia <elcorreia45@gmail.com>
2026-07-01 00:06:19 -0400

    build(docs): use published builder generation

--- build.js
+++ build.js
@@ -1,13 +1,19 @@
 import { buildModules, buildBrowser, buildDocs } from '@beforesemicolon/builder'
 
+const docsOptions = {
+    template: 'fading-citrus',
+    siteUrl: 'https://markup.beforesemicolon.com',
+    generatedFiles: {
+        netlify: true,
+    },
+}
+
 const run = async () => {
     try {
         await Promise.all([
             buildBrowser(),
             buildModules(),
-            buildDocs({
-                template: 'fading-citrus',
-            }),
+            buildDocs(docsOptions),
         ])
         // Wait for the unawaited async writeFile calls in builder's forEach to finish
         await new Promise((resolve) => setTimeout(resolve, 1000))
