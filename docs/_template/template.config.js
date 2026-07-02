fe4a31c0ae40cec5c33797f713e601771f53b1ff
Elson Correia <elcorreia45@gmail.com>
2026-07-02 01:09:19 -0400

    Configure docs social image metadata

---
+++ docs/_template/template.config.js
@@ -0,0 +1,9 @@
+export default {
+    meta: {
+        siteName: 'Markup',
+        title: 'Markup by Before Semicolon',
+        description:
+            'Markup is a tiny reactive HTML templating system for building JavaScript user interfaces with web standards, no JSX, and no build step.',
+        image: '/assets/markup-banner.jpg',
+    },
+}
