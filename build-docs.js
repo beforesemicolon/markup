651c362d1d76b50b4c749682d8bf5825d7a62d72
Elson Correia <elcorreia45@gmail.com>
2026-07-02 20:52:25 -0400

    Add legacy docs redirects

--- build-docs.js
+++ build-docs.js
@@ -10,6 +10,27 @@ const docsOptions = {
     },
 }
 
+const legacyRedirects = [
+    '/documentation/web-component https://web-component.beforesemicolon.com/ 301!',
+    '/documentation/web-component/ https://web-component.beforesemicolon.com/ 301!',
+    '/documentation/router https://router.beforesemicolon.com/ 301!',
+    '/documentation/router/ https://router.beforesemicolon.com/ 301!',
+]
+
+const appendLegacyRedirects = () => {
+    const redirectsPath = path.join(process.cwd(), 'website', '_redirects')
+    const redirects = fs.existsSync(redirectsPath)
+        ? fs.readFileSync(redirectsPath, 'utf8')
+        : ''
+    const nextRedirects = [
+        ...new Set([...redirects.split('\n'), ...legacyRedirects]),
+    ]
+        .filter(Boolean)
+        .join('\n')
+
+    fs.writeFileSync(redirectsPath, `${nextRedirects}\n`)
+}
+
 const run = async () => {
     try {
         fs.rmSync(path.join(process.cwd(), 'website'), {
@@ -20,6 +41,7 @@ const run = async () => {
         await buildDocs(docsOptions)
         // Wait for the unawaited async writeFile calls in builder's forEach to finish
         await new Promise((resolve) => setTimeout(resolve, 1000))
+        appendLegacyRedirects()
     } catch (e) {
         console.error(e)
     }
