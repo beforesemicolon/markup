023d7f6e7f7cdc1568abaa70b9405e9cb6242799
Elson Correia <elcorreia45@gmail.com>
2026-08-08 19:53:09 -0400

    fix legacy redirects

--- build-docs.js
+++ build-docs.js
@@ -2,29 +2,6 @@ import { buildDocs } from '@beforesemicolon/site-builder'
 import fs from 'fs'
 import path from 'path'
 
-const legacyRedirects = [
-    '/documentation/capabilities/web-component https://web-component.beforesemicolon.com/ 301!',
-    '/documentation/capabilities/web-component/ https://web-component.beforesemicolon.com/ 301!',
-    '/documentation/capabilities/router https://router.beforesemicolon.com/ 301!',
-    '/documentation/capabilities/router/ https://router.beforesemicolon.com/ 301!',
-    '/documentation/capabilities/form-controls https://web-component.beforesemicolon.com/documentation/advanced/form-integration 301!',
-    '/documentation/capabilities/form-controls/ https://web-component.beforesemicolon.com/documentation/advanced/form-integration 301!',
-]
-
-const appendLegacyRedirects = () => {
-    const redirectsPath = path.join(process.cwd(), 'website', '_redirects')
-    const redirects = fs.existsSync(redirectsPath)
-        ? fs.readFileSync(redirectsPath, 'utf8')
-        : ''
-    const nextRedirects = [
-        ...new Set([...redirects.split('\n'), ...legacyRedirects]),
-    ]
-        .filter(Boolean)
-        .join('\n')
-
-    fs.writeFileSync(redirectsPath, `${nextRedirects}\n`)
-}
-
 const run = async () => {
     try {
         fs.rmSync(path.join(process.cwd(), 'website'), {
@@ -33,7 +10,6 @@ const run = async () => {
         })
 
         await buildDocs()
-        appendLegacyRedirects()
     } catch (e) {
         console.error(e)
     }
--- build.js
+++ build.js
@@ -1,35 +1,9 @@
 import { buildModules, buildBrowser } from '@beforesemicolon/builder'
 import { buildDocs } from '@beforesemicolon/site-builder'
-import fs from 'fs'
-import path from 'path'
-
-const legacyRedirects = [
-    '/documentation/capabilities/web-component https://web-component.beforesemicolon.com/ 301!',
-    '/documentation/capabilities/web-component/ https://web-component.beforesemicolon.com/ 301!',
-    '/documentation/capabilities/router https://router.beforesemicolon.com/ 301!',
-    '/documentation/capabilities/router/ https://router.beforesemicolon.com/ 301!',
-    '/documentation/capabilities/form-controls https://web-component.beforesemicolon.com/documentation/advanced/form-integration 301!',
-    '/documentation/capabilities/form-controls/ https://web-component.beforesemicolon.com/documentation/advanced/form-integration 301!',
-]
-
-const appendLegacyRedirects = () => {
-    const redirectsPath = path.join(process.cwd(), 'website', '_redirects')
-    const redirects = fs.existsSync(redirectsPath)
-        ? fs.readFileSync(redirectsPath, 'utf8')
-        : ''
-    const nextRedirects = [
-        ...new Set([...redirects.split('\n'), ...legacyRedirects]),
-    ]
-        .filter(Boolean)
-        .join('\n')
-
-    fs.writeFileSync(redirectsPath, `${nextRedirects}\n`)
-}
 
 const run = async () => {
     try {
         await Promise.all([buildBrowser(), buildModules(), buildDocs()])
-        appendLegacyRedirects()
     } catch (e) {
         console.error(e)
     }
